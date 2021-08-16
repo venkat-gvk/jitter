53fee4ab8a1932376a0e85e578262990e949f04a
venkat-gvk <gvk496@gmail.com>
2021-08-16 19:10:02 +0530

    fixed height

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   const history = useHistory();
 
   return (
-    <div className=" min-h-screen flex flex-col justify-center items-center space-y-6">
+    <div className="h-full relative top-20 sm:static sm:min-h-screen flex flex-col justify-center items-center space-y-6">
       <div className="text-[#ff9d1e] text-center space-y-4">
         <p className="text-5xl">Welcome to Jitter</p>
         <p className="text-3xl text-white">A fun way to Chat</p>
