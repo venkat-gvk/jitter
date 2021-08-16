1e82024b2a1b04302dd160bba7606d101273e810
venkat-gvk <gvk496@gmail.com>
2021-08-16 12:46:00 +0530

    added env variables

--- src/components/firebase/config.js
+++ src/components/firebase/config.js
@@ -2,12 +2,12 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAdJKGd-0qVJJ5nHYild2f4rCulRdSW9bI",
-  authDomain: "jitter-fbfd3.firebaseapp.com",
-  projectId: "jitter-fbfd3",
-  storageBucket: "jitter-fbfd3.appspot.com",
-  messagingSenderId: "1064487694702",
-  appId: "1:1064487694702:web:5a12576fa67a1eb982517f",
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_SENDERID,
+  appId: process.env.REACT_APP_APPID,
 };
 
 const app = firebase.initializeApp(firebaseConfig);
